909c27a72ce5b520ca78be2402fbdfbd10dabca0
rayanemferreira <rmf.snf22@uea.edu.br>
2023-08-29 20:56:37 -0400

    adicionando codigos

---
+++ arvore 2-3/main.c
@@ -0,0 +1,31 @@
+#include "tree.h"
+
+int main(int argc, char *argv)
+{
+    TreeNode *arvore;
+    arvore = create(10, NULL);
+
+    int inserir[] = {5,
+                     15,
+                     3,
+                     7, 12, 18, 2, 4, 14};
+
+    for (int i = 0; i < 9; i++)
+    {
+        arvore = insert(inserir[i], arvore);
+    }
+
+    printf("\nÁrvore:\n");
+    imprime(arvore, 0);
+
+    removeMin(arvore, 5);
+    removeMin(arvore, 15);
+    // removeMin(arvore, 2);
+
+    // removeMin(arvore, 15);
+
+    printf("\nÁrvore:\n");
+    imprime(arvore, 0);
+
+    return 0;
+}
---
+++ arvore 2-3/tree.h
@@ -0,0 +1,382 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct TreeNode TreeNode;
+typedef struct link Link;
+
+TreeNode *balancear(TreeNode *aux);
+TreeNode *insert_in_root(int key, TreeNode *arvore);
+TreeNode *create(int key, TreeNode *dad);
+
+struct TreeNode
+{
+    int keys[3];
+    int num_keys;
+    TreeNode *children[3]; // children[menor que todas as chaves, entre as chaves, maior qur todas as chaves]
+    TreeNode *father;
+};
+
+TreeNode *insert(int key, TreeNode *arvore)
+{
+    TreeNode *aux = arvore;
+    int find_pos = 1;
+    // Caso a inserção ocorre na nó raiz da arvore
+    if (aux->father == NULL && aux->children[0] == NULL && aux->children[1] == NULL && aux->children[2] == NULL)
+    {
+        return insert_in_root(key, arvore);
+    }
+
+    // Caso não ,inicia um loop buscando uma posição na arvore
+    while (find_pos)
+    {
+        // Se a chave a ser inseria é menor que a chave de menor valor do nó pai, então vai para o galho esquerdo se esse for não nulo
+        if (key < aux->keys[0] && aux->children[0] != NULL)
+        {
+            aux = aux->children[0];
+        }
+        // Se a chave a ser inseria é maior que a chave de menor valor do nó pai e menor que a chave de maior valor do nó pai, então vai para o galho intermediário se esse for não nulo
+        else if (key > aux->keys[0] && key < aux->keys[1] && aux->children[1] != NULL)
+        {
+            aux = aux->children[1];
+        }
+        // Se a chave a ser inseria é maior que a chave de maior valor do nó pai, então vai para o galho direito se esse for não nulo
+        else if (key > aux->keys[2] && aux->children[2] != NULL)
+        {
+            aux = aux->children[2];
+        }
+        // Caso encontre um  nó não nulo, e com apenas uma chave, então adicione key a esse nó.
+        else if (aux->num_keys == 1)
+        {
+            aux->keys[1] = key;
+            aux->num_keys += 1;
+            aux = balancear(aux);
+            find_pos = 0;
+        }
+        // Caso encontre um nó não nulo, com duas chaves nele.
+        else if (aux->num_keys == 2 && aux->father != NULL)
+        {
+            TreeNode *temp;
+            aux = insert_in_root(key, aux);
+            // Inseri um valor no nó da arvore, e se o nó atingir tamanho 3, então divide os em três nós
+            /*
+            Exemplo: no(2,3), insiro o 4, então fica no(2,3,4), isso não pode ocorrer, logo é transformado em:
+            O novo nó pai:              ------3------
+                                        |           |
+            Os novos nós filhos:        2           4
+            */
+            temp = aux->father;
+
+            // Caso esse nó tenha um nó pai simples(apenas uma chave)
+            if (temp->num_keys == 1)
+            {
+                // Caso esse nó seja maior que a chave do nó pai
+                if (aux->keys[0] > temp->keys[0])
+                {
+                    temp->keys[1] = aux->keys[0];
+                    temp->children[1] = aux->children[0];
+                    temp->children[1]->father = temp;
+                    temp->children[2] = aux->children[2];
+                    temp->children[2]->father = temp;
+                    temp->num_keys += 1;
+                    free(aux);
+                    aux = NULL;
+                }
+
+                // Caso esse nó seja menor que a chave do nó pai
+                else if (aux->keys[0] < temp->keys[0])
+                {
+                    temp->keys[1] = temp->keys[0];
+                    temp->keys[0] = aux->keys[0];
+
+                    temp->children[0] = aux->children[0];
+                    temp->children[0]->father = temp;
+                    temp->children[1] = aux->children[2];
+                    temp->children[1]->father = temp;
+                    temp->num_keys += 1;
+
+                    free(aux);
+                    aux = NULL;
+                }
+            }
+
+            // Caso o nó possui um nó pai duplo
+            else if (temp->num_keys == 2)
+            {
+                TreeNode *temp_2;
+
+                // Caso a chave seja maior que o nó de maior valor do nó pai
+                if (aux->keys[0] > temp->keys[1])
+                {
+
+                    temp_2 = create(temp->keys[0], temp);
+                    temp_2->keys[0] = temp->keys[0];
+
+                    temp->keys[0] = temp->keys[1];
+                    temp->keys[1] = 0;
+
+                    temp_2->children[0] = temp->children[0];
+                    temp_2->children[2] = temp->children[1];
+
+                    temp_2->children[0]->father = temp_2;
+                    temp_2->children[2]->father = temp_2;
+
+                    temp->children[0] = temp_2;
+                    temp->children[2] = aux;
+                    temp->children[1] = NULL;
+
+                    temp->num_keys = 1;
+                }
+                // Caso a chave seja menor que o nó de menor valor do nó pai
+                else if (aux->keys[0] < temp->keys[0])
+                {
+                    temp_2 = create(temp->keys[1], temp);
+                    temp_2->keys[0] = temp->keys[1];
+
+                    temp->keys[1] = 0;
+
+                    temp_2->children[0] = temp->children[1];
+                    temp_2->children[2] = temp->children[2];
+
+                    temp_2->children[0]->father = temp_2;
+                    temp_2->children[2]->father = temp_2;
+
+                    temp->children[0] = aux;
+                    temp->children[1] = NULL;
+
+                    temp->children[2] = temp_2;
+                    temp->num_keys = 1;
+                }
+                // Caso a chave seja intermediária
+                else if (aux->keys[0] > temp->keys[0] && aux->keys[0] < temp->keys[1])
+                {
+                    temp = insert_in_root(aux->keys[0], temp);
+                    temp->children[0]->children[2] = aux->children[0];
+                    temp->children[2]->children[0] = aux->children[2];
+                    temp->children[1] = NULL;
+                    free(aux);
+                }
+            }
+            find_pos = 0;
+        }
+    }
+    return arvore;
+}
+
+// Funções auxiliares
+
+TreeNode *create(int key, TreeNode *dad)
+{
+    TreeNode *temp;
+    temp = (TreeNode *)malloc(sizeof(TreeNode));
+    for (int i = 0; i < 3; i++)
+    {
+        temp->keys[i] = 0;
+        temp->children[i] = NULL;
+    }
+    temp->keys[0] = key;
+    temp->father = dad; //
+    temp->num_keys = 1;
+    return temp;
+}
+
+TreeNode *insert_in_root(int key, TreeNode *arvore)
+{
+    if (arvore->num_keys == 1)
+    {
+        arvore->num_keys += 1;
+        arvore->keys[1] = key;
+        arvore = balancear(arvore);
+        return arvore;
+    }
+    else if (arvore->num_keys == 2)
+    {
+        arvore->num_keys += 1;
+        arvore->keys[2] = key;
+        arvore = balancear(arvore);
+
+        TreeNode *menor = (TreeNode *)malloc(sizeof(TreeNode));
+        TreeNode *maior = (TreeNode *)malloc(sizeof(TreeNode));
+
+        menor = create(arvore->keys[0], arvore);
+        maior = create(arvore->keys[2], arvore);
+
+        arvore->keys[0] = arvore->keys[1];
+        arvore->keys[1] = 0;
+        arvore->keys[2] = 0;
+        arvore->children[0] = menor;
+        arvore->children[2] = maior;
+        arvore->num_keys = 1;
+
+        return arvore;
+    }
+}
+
+TreeNode *balancear(TreeNode *aux)
+{
+    if (aux->num_keys == 2)
+    {
+        // Caso o nó tenha duas chaves, ordene-as
+        if (aux->keys[0] > aux->keys[1])
+        {
+            int temp = aux->keys[0];
+            aux->keys[0] = aux->keys[1];
+            aux->keys[1] = temp;
+        }
+        return aux;
+    }
+    else if (aux->num_keys == 3)
+    {
+        // Caso o nó tenha três chaves, ordene-as
+        if (aux->keys[0] > aux->keys[1])
+        {
+            int temp = aux->keys[0];
+            aux->keys[0] = aux->keys[1];
+            aux->keys[1] = temp;
+        }
+        if (aux->keys[1] > aux->keys[2])
+        {
+            int temp = aux->keys[1];
+            aux->keys[1] = aux->keys[2];
+            aux->keys[2] = temp;
+        }
+        if (aux->keys[0] > aux->keys[1])
+        {
+            int temp = aux->keys[0];
+            aux->keys[0] = aux->keys[1];
+            aux->keys[1] = temp;
+        }
+    }
+
+    return aux;
+}
+
+void imprime(TreeNode *root, int level)
+{
+    if (root == NULL)
+        return;
+
+    imprime(root->children[2], level + 1);
+    for (int i = 0; i < level; i++)
+        printf("    ");
+    for (int i = 0; i < root->num_keys; i++)
+        printf("%d ", root->keys[i]);
+    printf("\n");
+    imprime(root->children[1], level + 1);
+    imprime(root->children[0], level + 1);
+}
+
+TreeNode *removeNo(TreeNode *arvore)
+{
+    TreeNode *aux = arvore;
+    // printf("removeNo");
+    // no folha
+    if (aux->father != NULL && aux->children[0] == NULL && aux->children[1] == NULL && aux->children[2] == NULL && aux->keys[0] == 0 && aux->keys[1] == 0 && aux->keys[2] == 0)
+    {
+        printf("\n\nfree");
+
+        arvore = aux->father;
+        free(aux);
+    }
+
+    // no galho
+    if (aux->keys[0] == 0 && aux->keys[1] == 0 && aux->keys[2] == 0)
+    {
+        printf("\n\ngalho");
+        if (aux->children[0] != NULL)
+        {
+            aux->keys[0] = aux->children[0]->keys[0];
+            aux->children[0]->keys[0] = 0;
+            return removeNo(aux->children[0]);
+        }
+
+        else if (aux->children[1] != NULL)
+        {
+            aux->keys[1] = aux->children[1]->keys[0];
+            aux->children[1]->keys[0] = 0;
+            return removeNo(aux->children[1]);
+        }
+
+        else if (aux->children[2] != NULL)
+        {
+            aux->keys[2] = aux->children[2]->keys[0];
+            aux->children[2]->keys[0] = 0;
+            return removeNo(aux->children[2]);
+        }
+    }
+
+    return balancear(arvore);
+}
+
+void removeMin(TreeNode *arvore, int key)
+{
+
+    int find_pos2 = 1;
+    TreeNode *aux = arvore;
+
+    while (find_pos2)
+    {
+        // remover nó folha
+        if (key == aux->keys[0])
+        {
+            aux->keys[0] = 0;
+            // printf("\nachou");
+            aux->num_keys--;
+
+            aux = removeNo(aux);
+            find_pos2 = 0;
+        }
+        else if (key == aux->keys[1])
+        {
+            aux->keys[1] = 0;
+            aux->num_keys--;
+            // printf("\nachou");
+            aux = removeNo(aux);
+
+            find_pos2 = 0;
+        }
+
+        else if (key == aux->keys[2])
+        {
+            aux->keys[2] = 0;
+            aux->num_keys--;
+            // printf("\nachou");
+            aux = removeNo(aux);
+
+            find_pos2 = 0;
+        }
+        else if (key > aux->keys[0] && key < aux->keys[1] && aux->children[1] != NULL)
+        {
+            // printf("\nmeio");
+
+            aux = aux->children[1];
+        }
+        else if (key < aux->keys[0] && aux->children[0] != NULL)
+        {
+            // printf("\nesqu");
+
+            aux = aux->children[0];
+        }
+        else if (key > aux->keys[0] && aux->children[2] != NULL)
+        {
+            aux = aux->children[2];
+            // printf("\ndir");
+        }
+    }
+}
+//     TreeNode *aux;
+//     if (arvore == NULL)
+//     {
+
+//         return;
+//     }
+
+//     if (arvore->children[0] != NULL)
+//     {
+//         aux = arvore;
+//         arvore = arvore->children[1];
+//         free(aux);
+//         return arvore;
+//     }
+//     return arvore;
+
+// }
